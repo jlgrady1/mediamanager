a145fdc28a4d1f15c7fa85637fdcd1f097c32d3d
James Grady <james.grady@boomerain.com>
2013-05-24 17:31:23 -0400

    Added account and role models

--- account/models.py
+++ account/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Account(models.Model):
+    date_created = models.DateTimeField()
+    date_updated = models.DateTimeField()
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=255)
+
+class Role(models.Model):
+    date_created = models.DateTimeField()
+    date_updated = models.DateTimeField()
+    code = models.CharField(max_length=50)
+    parent = models.ForeignKey(Role)
