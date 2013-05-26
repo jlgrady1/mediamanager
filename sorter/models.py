6299bea0dc1c88d204edce93f73fa0c475e47688
James Grady <james.grady@boomerain.com>
2013-05-26 19:13:24 -0400

    defined sorter model

--- sorter/models.py
+++ sorter/models.py
@@ -1,4 +1,30 @@
 from django.db import models
 
+class Type(models.Model):
+    date_created = models.DateTimeField()
+    date_updated = models.DateTimeField()
+    code = models.CharField(max_length=100)
+
+class Status(models.Model):
+    date_created = models.DateTimeField()
+    date_updated = models.DateTimeField()
+    code = models.CharField(max_length=100)
+
+class Action(models.Model):
+    date_created = models.DateTimeField()
+    date_updated = models.DateTimeField()
+    mediafile = models.ForeignKey(MediaFile)
+    command = models.CharField(max_length=255)
+
 class MediaFile(models.Model):
-    
+    date_created = models.DateTimeField()
+    date_updated = models.DateTimeField()
+    type = models.ForeignKey(Type)
+    status = models.ForeignKey(Status)
+    filename = models.CharField(max_length=255)
+
+class DownloadFolder(models.Model):
+    date_created = models.DateTimeField()
+    date_updated = models.DateTimeField()
+    folder = models.CharField(max_length=255)
+    level = models.IntegerField()
