9d2ec05874cdc11b0e04e62492e70c6bc29bfd59
James Grady <james.grady@boomerain.com>
2013-06-05 09:45:03 -0400

    Added basic framework for celery
    -Using celery for task management and queuing

---
+++ sorter/tasks.py
@@ -0,0 +1,19 @@
+import logging
+import os
+
+from celery import task
+
+# Get an instance of a logger
+log = logging.getLogger(__name__)
+
+@task()
+def scan(media_folder):
+    folder = media_folder.folder
+    log.info(u'starting media scan of %s' % folder)
+    contents = os.listdir(folder)
+    log.debug("found media files: " + str(contents))
+    for file in contents:
+        os.path.isfile(file):
+            pass
+
+    return contents
